e999cd3c04094c1a516052174235ecddfa3f7540
mona.niknafs <mona.niknafs@hotmail.com>
2014-12-11 09:41:56 +0000

    Fix indentation (to 2 spaces)

--- lib/spmidi/data_collection.rb
+++ lib/spmidi/data_collection.rb
@@ -4,69 +4,69 @@ require 'unimidi'
 require_relative 'note'
 
 module SPMidi
-	class DataCollection
-		def runtime()
-			# KORG nanoKEY2 produces notes from C_octave5 down to C_octave3
-			# assuming MIDI keyboard is last input
-			input = UniMIDI::Input.last
-			input.clear_buffer
+  class DataCollection
+    def runtime()
+      # KORG nanoKEY2 produces notes from C_octave5 down to C_octave3
+      # assuming MIDI keyboard is last input
+      input = UniMIDI::Input.last
+      input.clear_buffer
 
-			# buff = Array.new
-			prev_ts = 0 # use for relative timestamp
-			start_ts = 0 # use to identify start of recording
+      # buff = Array.new
+      prev_ts = 0 # use for relative timestamp
+      start_ts = 0 # use to identify start of recording
 
-			# runtime behaviour
-			input.open do |input|
-				puts "give me some notes!"
-				record = false
-				buff = input.buffer
+      # runtime behaviour
+      input.open do |input|
+        puts "give me some notes!"
+        record = false
+        buff = input.buffer
         record_buffer = []
         index = 0
 
-				# blocks until input comes in
-				while m = input.gets[0]
+        # blocks until input comes in
+        while m = input.gets[0]
 
-					ts = m[:timestamp]
-					data = m[:data]
+          ts = m[:timestamp]
+          data = m[:data]
 
-					note = Note.new(data, ts - start_ts, ts - prev_ts, buff, index)
+          note = Note.new(data, ts - start_ts, ts - prev_ts, buff, index)
           prev_ts = ts
 
-					index += 1
+          index += 1
 
-					if record
-						if note.data == [176,64,0]
-							record = false
-							start_ts = ts
-							puts "stopped recording"
-							record_buffer.each {|x| x.sp_letter_print}
-						else
-		          if note.data[0] == 144
+          if record
+            if note.data == [176,64,0]
+              record = false
+              start_ts = ts
+              puts "stopped recording"
+              record_buffer.each {|x| x.sp_letter_print}
+            else
+              if note.data[0] == 144
                 note.lock_rel_ts(1/8.0)
-								record_buffer << note
-		          end
-						end
-					else
-					 	# not recording
-					 	# print only the on-notes
-					 	if note.data[0] == 144
+                record_buffer << note
+              end
+            end
+          else
+            # not recording
+            # print only the on-notes
+            if note.data[0] == 144
               note.sp_ambi_print
-					 	end
+            end
           end
 
-				 	# initialise recording if applicable
-					if note.data == [176,64,127] # on-value as specified by keyboard
-						record = true
+          # initialise recording if applicable
+          if note.data == [176,64,127] # on-value as specified by keyboard
+            record = true
             record_buffer = []
-						start_ts = ts
-						puts "started recording"
-					end
+            start_ts = ts
+            puts "started recording"
+          end
 
-				end
-			end
-		end
+        end
+      end
+    end
 
-		d = DataCollection.new
-		d.runtime()
-	end
+    d = DataCollection.new
+    d.runtime()
+  end
 end
--- lib/spmidi/note.rb
+++ lib/spmidi/note.rb
@@ -3,52 +3,52 @@ require 'set'
 require_relative 'samples'
 
 module SPMidi
-	class Note
-		attr_accessor :data, :ts, :rel_ts, :buff, :index
+  class Note
+    attr_accessor :data, :ts, :rel_ts, :buff, :index
 
-		def initialize(data, ts, rel_ts, buff, index)
-  		@data = data # an array [status, pitch, velocity]
-			@ts = ts # timestamp relative to input connection
-			@rel_ts = rel_ts # timestamp relative to prev note played
-			@buff = buff # input buffer note was initially played into
-			@index = index # index into input buffer
-		end
+    def initialize(data, ts, rel_ts, buff, index)
+      @data = data # an array [status, pitch, velocity]
+      @ts = ts # timestamp relative to input connection
+      @rel_ts = rel_ts # timestamp relative to prev note played
+      @buff = buff # input buffer note was initially played into
+      @index = index # index into input buffer
+    end
 
-		def down_octave(x=1) 
-			# default is to move down one octave as name suggests
-			old_note = @data[1]
-			new_note = old_note - x*12
+    def down_octave(x=1) 
+      # default is to move down one octave as name suggests
+      old_note = @data[1]
+      new_note = old_note - x*12
 
-			if new_note.between?(0,127)
-				return new_note
-			else
-				puts "can't move that far, check down_octave method inputs"
-				return old_note
-			end
-		end
+      if new_note.between?(0,127)
+        return new_note
+      else
+        puts "can't move that far, check down_octave method inputs"
+        return old_note
+      end
+    end
 
-		def note_letter()
-			# uses note_octave method seen below
-			# can i use @ with this and put it as 'global' variable?
-			note_array = [:C,:Cs,:D,:Ds,:E,:F,:Fs,:G,:Gs,:A,:As,:B]
-			note_name =  note_array[@data[1] % 12]
-			note_with_octave = "#{note_name}#{note_octave()}"
-			return note_with_octave
-		end
+    def note_letter()
+      # uses note_octave method seen below
+      # can i use @ with this and put it as 'global' variable?
+      note_array = [:C,:Cs,:D,:Ds,:E,:F,:Fs,:G,:Gs,:A,:As,:B]
+      note_name =  note_array[@data[1] % 12]
+      note_with_octave = "#{note_name}#{note_octave()}"
+      return note_with_octave
+    end
 
-		def note_octave()
-			# octaves start from 0 up to 10
-			# by MIDI note constraints
-			return @data[1] / 12
-		end
+    def note_octave()
+      # octaves start from 0 up to 10
+      # by MIDI note constraints
+      return @data[1] / 12
+    end
 
-		def lock_to_structure(rel_ts_or_ts, element)
-			# min_time_element in milliseconds
-			timestamp = (rel_ts_or_ts == "rel_ts" ? @rel_ts : @ts)
-			r = timestamp % element
-			rounded_ts = timestamp - r + (r >= element / 2.0 ? element : 0)
-			return rounded_ts
-		end
+    def lock_to_structure(rel_ts_or_ts, element)
+      # min_time_element in milliseconds
+      timestamp = (rel_ts_or_ts == "rel_ts" ? @rel_ts : @ts)
+      r = timestamp % element
+      rounded_ts = timestamp - r + (r >= element / 2.0 ? element : 0)
+      return rounded_ts
+    end
 
     def lock_rel_ts(element)
       @rel_ts = lock_to_structure("rel_ts", element)
@@ -105,5 +105,5 @@ module SPMidi
       end
     end
 
-	end
+  end
 end
