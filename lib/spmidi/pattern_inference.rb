272ead6cd901f2bd0cef4f9d5d06916893a93224
mona.niknafs <mona.niknafs@hotmail.com>
2015-01-15 11:37:28 +0000

    add prev array which keeps a record of all previously confirmed patterns

--- lib/spmidi/pattern_inference.rb
+++ lib/spmidi/pattern_inference.rb
@@ -3,13 +3,14 @@ require_relative 'pattern'
 
 module SPMidi
   class PatternInference
-    attr_accessor :current, :ph, :backlog, :matched
+    attr_accessor :current, :ph, :backlog, :matched, :prev
 
     def initialize()
+      @prev = [] # array of hash sets defining prev patterns
       @current = Pattern.new # current pattern
       @ph = 0 # @ph is an index into pattern
-      @backlog = [] # array of past seen notes
-      @matched = []# array of notes matched so far
+      @backlog = [] # array of past seen elements
+      @matched = []# array of elements matched so far
     end
 
     def inc_ph()
@@ -17,7 +18,7 @@ module SPMidi
         @ph += 1
       else
         @ph += 1
-        @ph = @ph % @current.notes.length
+        @ph = @ph % @current.elements.length
       end
     end
 
@@ -28,29 +29,38 @@ module SPMidi
       @matched = []
     end
 
-    def save(note)
-      @backlog << note
+    def save(element)
+      @backlog << element
     end
 
-    def match(note)
-      @matched << note
+    def prev_add(pattern)
+      if @prev.include?(pattern)
+        i = @prev.index(pattern)
+        @prev[i].confirm()
+      else
+        @prev << pattern
+      end
     end
 
-    def backlog_match(notes)
+    def match(element)
+      @matched << element
+    end
+
+    def backlog_match(elements)
       # naive string matching
       substring = []
-      mlen = notes.length
-      clen = @current.notes.length
+      mlen = elements.length
+      clen = @current.elements.length
       for i in 0..mlen-1
         for j in 0..clen-1
-          m = notes[i,mlen]
+          m = elements[i,mlen]
           mlen = m.length
-          c = @current.notes[j,mlen]
+          c = @current.elements[j,mlen]
           if m == c
             puts "i-index for matched = #{i}" # remove
             puts "j-index for matched = #{j}" # remove
-            substring = @current.notes[j,clen].dup
-            @matched = notes
+            substring = @current.elements[j,clen].dup
+            @matched = elements
             @ph = mlen-1
             return substring
           end
@@ -59,45 +69,48 @@ module SPMidi
       return substring
     end
 
-    def find_pattern_size(note)
+    def find_pattern_size(element)
       puts "pass number #{@index}" # remove
-      puts "note: #{note.data}" # remove
+      puts "element: #{element.data}" # remove
       puts "placeholder: #{ph}" # remove
 
-      save(note) # put note in backlog list
+      save(element) # put element in backlog list
 
       if @current.confidence == 0 
-        occurs = @current.occurs_after?(note,0) # occurrence in pattern
+        occurs = @current.occurs_after?(element,0) # occurrence in pattern
         if occurs != nil
           @current.trim_before(occurs)
-          puts note.class
-          match(note)
+          puts element.class
+          match(element)
           @current.confirm
+          prev_add(@current)
           puts "confirmed" # remove
         else
-          @current.add(note)
+          @current.add(element)
         end
       else # have already confirmed pattern
-        occurs = @current.occurs_after?(note, @ph)
+        occurs = @current.occurs_after?(element, @ph)
         if occurs == @ph
           puts "follows pattern" #remove
-          match(note)
+          match(element)
           if @ph+1 == @current.length
             @current.confirm
+            prev_add(@current)
           end
         else
           if occurs != nil
             (@matched).each {|x| p x.data} # remove
-            new_current = backlog_match(@matched.dup << note) 
+            new_current = backlog_match(@matched.dup << element) 
             puts "" # remove
-            (@matched.dup << note).each {|x| p x.data} # remove
+            (@matched.dup << element).each {|x| p x.data} # remove
             puts "follows pattern subset" # remove
-            (@backlog).each {|x| p x.data} # remove
+            @backlog.each {|x| p x.data} # remove
             puts "" # remove
             @current = Pattern.new(new_current.dup)
             @current.confirm()
+            prev_add(@current)
             (@matched).each {|x| p x.data} # remove
-            if @current.notes.length == 0 #remove
+            if @current.elements.length == 0 #remove
               puts "empty current pattern" #remove
             end #remove
           else
