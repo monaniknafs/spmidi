de56cb97b8799f3a8996ded6e9e0dfc2a9d1a86b
mona.niknafs <mona.niknafs@hotmail.com>
2015-02-07 11:09:59 +0000

    correct use of note to element

--- lib/spmidi/pattern.rb
+++ lib/spmidi/pattern.rb
@@ -3,12 +3,10 @@ require_relative 'pattern_element'
 module SPMidi
   class Pattern
     attr_accessor :elements, :confidence
-    # test intialization with note for pattern element
-    def initialize(notes = [])
+    def initialize(elements = [])
       @elements = [] # contains elements in pattern in order of occurrence
-      notes.each do |note|
-        e = PatternElement.new(note)
-        @elements << e
+      elements.each do |el|
+        @elements << el
       end
       @confidence = 0 # the number of times the pattern has been seen
     end
@@ -17,8 +15,8 @@ module SPMidi
       return @elements == pattern.elements
     end
 
-    def add(note)
-      @elements << PatternElement.new(note)
+    def add(element)
+      @elements << element
     end
 
     def trim_before(start)
@@ -32,32 +30,30 @@ module SPMidi
     def print()
       @elements.each do |e|
         p e.data
-        puts e.rel_ts
+        puts e.mean_ts
       end
     end
 
-    def occurs?(note, index)
-      # if pattern_element(note) is in @elements
+    def occurs?(element, index)
+      # if element is in @elements
         # return true
       # else returns false
       # method isn't used at the momento
       if index > @elements.length-1
         return false
       end
-      e = PatternElement.new(note)
-      return e == elements[index]
+      return element == elements[index]
     end
 
-    def occurs_after?(note,index) 
-      # if note occurs from point index onwards
+    def occurs_after?(element,index) 
+      # if element occurs from point index onwards
         # return index of first occurrence
       # nil returned if false or invalid
       if index > @elements.length-1
         return nil
       end
-      e = PatternElement.new(note)
       for i in index..@elements.length-1
-        if @elements[i] == e
+        if @elements[i] == element
             return i
         end
       end
