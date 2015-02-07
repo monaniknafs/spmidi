67ae9a85de2aacf507a6e4a8bf1c51d19c463b89
mona.niknafs <mona.niknafs@hotmail.com>
2015-02-07 11:09:26 +0000

    remove rel_ts from definition, now use relative timestamp distribution

--- lib/spmidi/pattern_element.rb
+++ lib/spmidi/pattern_element.rb
@@ -1,32 +1,50 @@
 module SPMidi
   class PatternElement
-    attr_accessor :data, :rel_ts
+    attr_reader :data, :timestamps, :mean_ts, :n 
 
     def initialize(*args)
+      @n = 1 # number of (rel) timestamp elements deemed equal
       if args.size == 2
         @data = args[0]
-        @rel_ts = args[1]
+        @mean_ts = args[1]
+        @timestamps = [@mean_ts]
       elsif args.size == 1
         @data = args[0].data
-        @rel_ts = args[0].rel_ts
+        @mean_ts = args[0].rel_ts
+        @timestamps = [@mean_ts]
       else
         puts 'error: pattern element initialize takes 1 or 2 arguments'
       end
     end
-    
+
     def ==(element)
+      if element == nil
+        return false
+      end
       if element.data[1] != @data[1]
         return false
       end
       # rel tstamps are normally distributed
-      # assume @rel_ts is equal to its mean
-      # following, its approx standard deviation:
-      sd = 44 + 0.0447*(@rel_ts - 637)
-      if (element.rel_ts >= @rel_ts - sd && element.rel_ts <= @rel_ts + sd)
+      # DECISION: underlying timestamps are in MIDI units
+      # sonic pi units can come into play in the higher level
+      # using line of regression, approx standard deviation:
+      sd = 44 + 0.0447*(@mean_ts - 637)
+      if (element.mean_ts >= @mean_ts - sd && element.mean_ts <= @mean_ts + sd)
+        # element is defined equal when within one standard deviatation
+        # TODO: revise this assumption, by looking at distributions of sample data sets
+        prev_mean_ts = @mean_ts
+        @n += 1
+        @timestamps << element.mean_ts
+        # equation from mmethods supervision exercises
+        @mean_ts = prev_mean_ts + (element.mean_ts - prev_mean_ts)/@n # is this a risky subtraction to be doing?
         return true
       else
         return false
       end 
+    end
+
+    def print
+      puts "data: #{data}, rel ts: #{mean_ts}"
     end
   end
 end
