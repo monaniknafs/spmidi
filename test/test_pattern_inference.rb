771293e0d583bad3ef89e176f015b2e313437941
mona.niknafs <mona.niknafs@hotmail.com>
2015-02-07 11:07:21 +0000

    rewrite pattern_inference test, incorrectly used notes as oppose to elements

--- test/test_pattern_inference.rb
+++ test/test_pattern_inference.rb
@@ -10,33 +10,33 @@ module SPMidi
       # p*_notes is array of notes in repeated pattern
 
       # test 1
-      notes1 = [
-      Note.new([2,3,4],0.0,200.0),
-      Note.new([4,5,6],0.1,120.1),
-      Note.new([1,2,3],0.7,100.3),
-      Note.new([1,7,4],0.5,110.3),
-      Note.new([2,3,4],0.0,200.0),
-      Note.new([4,5,6],0.1,120.1),
-      Note.new([1,2,3],0.7,100.3),
-      Note.new([1,7,4],0.5,110.3),
-      Note.new([2,3,4],0.0,200.0),
-      Note.new([4,5,6],0.1,120.1),
-      Note.new([1,2,3],0.7,100.3),
-      Note.new([1,7,4],0.5,110.3),
-      Note.new([2,3,4],0.0,200.0),
-      Note.new([4,5,6],0.1,120.1),
-      Note.new([1,2,3],0.7,100.3),
-      Note.new([1,7,4],0.5,110.3)]
+      elements1 = [
+      PatternElement.new([2,3,4],200.0),
+      PatternElement.new([4,5,6],120.1),
+      PatternElement.new([1,2,3],100.3),
+      PatternElement.new([1,7,4],110.3),
+      PatternElement.new([2,3,4],200.0),
+      PatternElement.new([4,5,6],120.1),
+      PatternElement.new([1,2,3],100.3),
+      PatternElement.new([1,7,4],110.3),
+      PatternElement.new([2,3,4],200.0),
+      PatternElement.new([4,5,6],120.1),
+      PatternElement.new([1,2,3],100.3),
+      PatternElement.new([1,7,4],110.3),
+      PatternElement.new([2,3,4],200.0),
+      PatternElement.new([4,5,6],120.1),
+      PatternElement.new([1,2,3],100.3),
+      PatternElement.new([1,7,4],110.3)]
 
-      p_notes1 = [
-      Note.new([2,3,4],0.0,200.0),
-      Note.new([4,5,6],0.1,120.1),
-      Note.new([1,2,3],0.7,100.3),
-      Note.new([1,7,4],0.5,110.3)]
+      p_elements1 = [
+      PatternElement.new([2,3,4],200.0),
+      PatternElement.new([4,5,6],120.1),
+      PatternElement.new([1,2,3],100.3),
+      PatternElement.new([1,7,4],110.3)]
 
       pi1 = PatternInference.new
-      notes1.each do |note1|
-        pi1.find_pattern_size(note1)
+      elements1.each do |el|
+        pi1.find_pattern_size(el)
       end
 
       # remove
@@ -46,56 +46,56 @@ module SPMidi
       end
       # remove
 
-      assert_equal(p_notes1.length, pi1.current.elements.length)
+      assert_equal(p_elements1.length, pi1.current.elements.length)
       for i in 0..pi1.current.elements.length-1
-        assert_equal(p_notes1[i].rel_ts, pi1.current.elements[i].rel_ts)
-        assert_equal(p_notes1[i].data, pi1.current.elements[i].data)
+        assert_equal(p_elements1[i].mean_ts, pi1.current.elements[i].mean_ts)
+        assert_equal(p_elements1[i].data, pi1.current.elements[i].data)
       end
 
       # test 2
-      notes2 = [
-      Note.new([2,3,4],0.0,200.0),
-      Note.new([2,3,4],0.0,200.0),
-      Note.new([2,3,4],0.0,200.0),
-      Note.new([4,5,6],0.1,120.1),
-      Note.new([4,5,6],0.1,120.1),
-      Note.new([2,3,4],0.0,200.0),
-      Note.new([2,3,4],0.0,200.0),
-      Note.new([2,3,4],0.0,200.0),
-      Note.new([4,5,6],0.1,120.1),
-      Note.new([4,5,6],0.1,120.1),
-      Note.new([2,3,4],0.0,200.0),
-      Note.new([4,5,6],0.1,120.1),
-      Note.new([5,2,4],0.5,183.3),
-      Note.new([5,2,4],0.5,183.3),
-      Note.new([1,7,4],0.5,110.3),
-      Note.new([2,3,4],0.0,200.0),
-      Note.new([4,5,6],0.1,120.1),
-      Note.new([5,2,4],0.5,183.3),
-      Note.new([5,2,4],0.5,183.3),
-      Note.new([1,7,4],0.5,110.3),
-      Note.new([2,3,4],0.0,200.0),
-      Note.new([4,5,6],0.1,120.1),
-      Note.new([5,2,4],0.5,183.3),
-      Note.new([5,2,4],0.5,183.3),
-      Note.new([1,7,4],0.5,110.3),
-      Note.new([2,3,4],0.0,200.0),
-      Note.new([4,5,6],0.1,120.1),
-      Note.new([5,2,4],0.5,183.3),
-      Note.new([5,2,4],0.5,183.3),
-      Note.new([1,7,4],0.5,110.3)]
+      elements2 = [
+      PatternElement.new([2,3,4],200.0),
+      PatternElement.new([2,3,4],200.0),
+      PatternElement.new([2,3,4],200.0),
+      PatternElement.new([4,5,6],120.1),
+      PatternElement.new([4,5,6],120.1),
+      PatternElement.new([2,3,4],200.0),
+      PatternElement.new([2,3,4],200.0),
+      PatternElement.new([2,3,4],200.0),
+      PatternElement.new([4,5,6],120.1),
+      PatternElement.new([4,5,6],120.1),
+      PatternElement.new([2,3,4],200.0),
+      PatternElement.new([4,5,6],120.1),
+      PatternElement.new([5,2,4],183.3),
+      PatternElement.new([5,2,4],183.3),
+      PatternElement.new([1,7,4],110.3),
+      PatternElement.new([2,3,4],200.0),
+      PatternElement.new([4,5,6],120.1),
+      PatternElement.new([5,2,4],183.3),
+      PatternElement.new([5,2,4],183.3),
+      PatternElement.new([1,7,4],110.3),
+      PatternElement.new([2,3,4],200.0),
+      PatternElement.new([4,5,6],120.1),
+      PatternElement.new([5,2,4],183.3),
+      PatternElement.new([5,2,4],183.3),
+      PatternElement.new([1,7,4],110.3),
+      PatternElement.new([2,3,4],200.0),
+      PatternElement.new([4,5,6],120.1),
+      PatternElement.new([5,2,4],183.3),
+      PatternElement.new([5,2,4],183.3),
+      PatternElement.new([1,7,4],110.3)]
 
-      p_notes2 = [
-      Note.new([2,3,4],0.0,200.0),
-      Note.new([4,5,6],0.1,120.1),
-      Note.new([5,2,4],0.5,183.3),
-      Note.new([5,2,4],0.5,183.3),
-      Note.new([1,7,4],0.5,110.3)]
+      p_elements2 = [
+      PatternElement.new([2,3,4],200.0),
+      PatternElement.new([4,5,6],120.1),
+      PatternElement.new([5,2,4],183.3),
+      PatternElement.new([5,2,4],183.3),
+      PatternElement.new([1,7,4],110.3)]
 
       pi2 = PatternInference.new
 
-      notes2.each do |note2|
-        pi2.find_pattern_size(note2)
+      elements2.each do |el|
+        pi2.find_pattern_size(el)
       end
 
       # remove
@@ -106,10 +106,10 @@ module SPMidi
       end
       # remove
 
-      assert_equal(p_notes2.length, pi2.current.elements.length)
+      assert_equal(p_elements2.length, pi2.current.elements.length)
       for i in 0..pi2.current.elements.length-1
-        assert_equal(p_notes2[i].rel_ts, pi2.current.elements[i].rel_ts)
-        assert_equal(p_notes2[i].data, pi2.current.elements[i].data)
+        assert_equal(p_elements2[i].mean_ts, pi2.current.elements[i].mean_ts)
+        assert_equal(p_elements2[i].data, pi2.current.elements[i].data)
       end
     end
   end
