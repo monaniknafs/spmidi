a34c0b34d2dd0b4d276b335037034e856beb1c48
mona.niknafs <mona.niknafs@hotmail.com>
2015-02-07 11:06:49 +0000

    new skeleton class, defines the structure of transition probabilities and emission probabilities

--- lib/spmidi/skeleton.rb
+++ lib/spmidi/skeleton.rb
@@ -1,47 +1,28 @@
-require 'set'
+require_relative 'note'
 require_relative 'pattern_element'
 
 module SPMidi
   class Skeleton
-    @prev_el
-    @skeleton # Hash of elements of form.. see below
-    # set of hash elements
-    # each element of form {pattern_element => goes_to_set}
-    # i call this a joint :/
-    attr_reader :skeleton
+    attr_reader :joints, :processed # data structure? array? hash?
 
     def initialize
-      @skeleton = Hash.new
-      @prev_el = nil
+      @joints = ##
+      # whether probabilities have been added
+      @processed = false 
     end
 
-    def add(note)
-      # notes are internally changed to pattern element bigrams
-      # represent bigrams implictly, 
-      # as Hash element {..,pattern_element => to_pointers_set,..}
+    def merge(joint)
+      # joint is Hash  
+      # form {pe => {pe1 => pr1, .., pen => prn}}
+    end
 
-      pe = PatternElement.new(note)
-      if @prev_el == nil # this should only happen on first call to function
-        @prev_el = pe.dup # do i need to dup? i wouldn't think so..
-      else
-        el = @prev_el.dup
-        @prev_el = pe.dup # next method call's bigram head
+    def process
+      # at end
+      @processed = true
+    end
 
-        s = Hash.new
-        merged = false
-        @skeleton.each do |key, val|
-          if key == el
-            s[key] = val.merge([pe].to_set)
-            merged = true
-          else 
-            s[key] = val
-          end
-        end
-        if !merged
-          s[el] = [pe].to_set
-        end
-        @skeleton = s
-      end
+    def transition(root, destn)
+
     end
   end
 end
