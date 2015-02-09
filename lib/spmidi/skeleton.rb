94e15d8b345d6e51b8743636320e7c2c3e326b9e
mona.niknafs <mona.niknafs@hotmail.com>
2015-02-09 11:20:13 +0000

    complete skeleton class

--- lib/spmidi/skeleton.rb
+++ lib/spmidi/skeleton.rb
@@ -3,26 +3,41 @@ require_relative 'pattern_element'
 
 module SPMidi
   class Skeleton
-    attr_reader :joints, :processed # data structure? array? hash?
+    attr_reader :joints 
 
     def initialize
-      @joints = ##
-      # whether probabilities have been added
-      @processed = false 
+      @joints = {}
     end
 
-    def merge(joint)
-      # joint is Hash  
+    def merge(root, destn, trans=false)
       # form {pe => {pe1 => pr1, .., pen => prn}}
-    end
 
-    def process
-      # at end
-      @processed = true
-    end
-
-    def transition(root, destn)
-
+      # will need to search through all the joints for matching root
+      merged_root = false
+      @joints.each do |rt, dns|
+        if rt.eql?(root) # so elements aren't in distbn twice
+          merged_root = true
+          # merge into joints
+          # DESIGN DECISION: do worry about repeat p_elements
+          # TODO ^^
+          if trans == true
+            merged_trans = false
+            dns.each do |d, pr|
+              if d==destn
+                merged_trans = true
+              end 
+            end
+            if !merged_trans
+              dns[destn] = 0.0
+            end
+          else
+            dns[destn] = 0.0
+          end
+        end
+      end
+      if !merged_root
+        @joints[root] = {destn => 0.0}
+      end
     end
   end
 end
