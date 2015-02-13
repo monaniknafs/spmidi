0ef454af0827e6820aa62dc0a5e2657b1af70aaa
mona.niknafs <mona.niknafs@hotmail.com>
2015-02-13 11:36:28 +0000

    new methods added for use of more skeleton instances in viterbi class, not sure if these new methods are consistent with the rest yet

--- lib/spmidi/skeleton.rb
+++ lib/spmidi/skeleton.rb
@@ -9,7 +9,7 @@ module SPMidi
       @joints = {}
     end
 
-    def merge(root, destn, trans=false)
+    def merge(root, destn, nested_destn, trans=false)
       # form {pe => {pe1 => pr1, .., pen => prn}}
 
       # will need to search through all the joints for matching root
@@ -18,26 +18,44 @@ module SPMidi
         if rt.eql?(root) # so elements aren't in distbn twice
           merged_root = true
           # merge into joints
-          # DESIGN DECISION: do worry about repeat p_elements
-          # TODO ^^
           if trans == true
             merged_trans = false
-            dns.each do |d, pr|
+            dns.each do |d, nd| # nd stands for nested destination
               if d==destn
                 merged_trans = true
               end 
             end
             if !merged_trans
-              dns[destn] = 0.0
+              dns[destn] = nested_destn
             end
           else
-            dns[destn] = 0.0
+            dns[destn] = nested_destn
           end
         end
       end
       if !merged_root
-        @joints[root] = {destn => 0.0}
+        @joints[root] = {destn => nested_destn}
       end
+    end
+
+    # used for viterbi
+    # so doesn't need merging atm
+
+    def get_inner_destn(root, destn)
+      @joints.each do |rt, dns|
+        if rt.eql?(root)
+          dns.each do |d, nd| # nd stands for nested destination
+            if d.eql?(destn)
+              return nd
+            end
+          end
+        end
+      end
+    end
+
+    def set_inner_destn(root, destn, value)
+      # ASSERT: root => {destn => __} not present in joints
+      @joints[rt][destn] = value
     end
   end
 end
