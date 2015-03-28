28b8efaa6a4ed3281530c5f0484ce9e01396214c
mona.niknafs <mona.niknafs@hotmail.com>
2015-03-28 12:38:44 +0000

    add wildcard pitch method for detection of type 1 and type 2 errors, with associated functionality

--- lib/spmidi/pattern_element.rb
+++ lib/spmidi/pattern_element.rb
@@ -1,27 +1,35 @@
 module SPMidi
   class PatternElement
-    attr_reader :data, :timestamps, :mean_ts, :sd, :n, :th
+    attr_reader :data, :timestamps, :mean_ts, :sd, :n, :th, :wild_pitch
 
     def initialize(*args)
       if args.size == 2
         @data = args[0]
         @mean_ts = args[1]
-        @timestamps = [@mean_ts]
       elsif args.size == 1
         @data = args[0].data
         @mean_ts = args[0].rel_ts
-        @timestamps = [@mean_ts]
       else
         puts 'error: pattern element initialize takes 1 or 2 arguments'
+        return
       end
+      @timestamps = [@mean_ts]
+      @wild_pitch = !@data ? true : false
+
+      # test whether we have wildcard pitch
       @n = 1 # number of (rel) timestamp elements deemed equal
       @sd = 44 + 0.0447*(@mean_ts - 637)
-      @th = 3.0 # num of standard deviations around mean_ts tolerated for equality of ts
+      @th = 2.0 # num of standard deviations around mean_ts tolerated for equality of ts
     end
 
     def ==(element)
       if element == nil
         return false
+      end
+      if @wild_pitch 
+        puts "equality method == can't be used with wild pitch pattern elements"
+        puts "can only be used with eql? method"
+        return
       end
       if element.data[1] != @data[1]
         return false
@@ -50,8 +58,10 @@ module SPMidi
       if element == nil
         return false
       end
-      if element.data[1] != @data[1]
-        return false
+      if !@wild_pitch && !element.wild_pitch
+        if element.data[1] != @data[1]
+          return false
+        end
       end
       # rel tstamps are normally distributed,
       # using line of regression, approx standard deviation:
@@ -64,7 +74,11 @@ module SPMidi
     end
 
     def print
-      puts "data: #{data}, rel ts: #{mean_ts}"
+      if @wild_pitch
+        puts "data: *, rel ts: #{mean_ts}"
+      else
+        puts "data: #{data}, rel ts: #{mean_ts}"
+      end
     end
   end
 end
