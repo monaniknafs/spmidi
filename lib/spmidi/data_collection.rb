c0812352fd16301c9ada635d78e117b6e89056cc
mona.niknafs <mona.niknafs@hotmail.com>
2015-04-15 15:08:19 +0100

    function calls added to call viterbi inference

--- lib/spmidi/data_collection.rb
+++ lib/spmidi/data_collection.rb
@@ -3,6 +3,8 @@ require 'unimidi'
 require_relative 'note'
 require_relative 'pattern_element'
 require_relative 'pattern_inference'
+require_relative 'hmm'
+require_relative 'viterbi'
 
 module SPMidi
   class DataCollection
@@ -16,13 +18,13 @@ module SPMidi
       prev_ts = 0 # use for relative timestamp
       start_ts = 0 # use to identify start of recording
 
-      pi = PatternInference.new
+      hmm = HMM.new
 
       # runtime behaviour
       input.open do |input|
         puts "give me some notes!"
         record = false
-        record_buffer = []
+        record_buffer = [] # array of notes
 
         # blocks until input comes in
         while m = input.gets[0]
@@ -44,16 +46,32 @@ module SPMidi
               record = false
               start_ts = ts
               puts "stopped recording"
-              # record_buffer.each {|x| x.sp_letter_print}
+
+              hmm.process
+
+              viterbi = Viterbi.new(record_buffer,hmm)
+              viterbi.robust(0.09) # adjustable variable
+              # why aren't these all the same method already?
+              viterbi.run
+              viterbi.find_path
+              viterbi.print_path
+
+              pi = PatternInference.new
+              viterbi.el_path.each do |el|
+                pi.find_pattern(el)
+              end
+
               puts "inferrred pattern:"
               pi.current.elements.each do |e|
-                e.print
+                e.set_sp_ts
+                locked = PatternElement.new(e.data,e.lock_sp_ts(1/16.0))
+                locked.sp_drum_print
               end
             else
               if note.data[0] == 144
-                note.lock_rel_ts(1/8.0)
+                # note.lock_rel_ts(1/8.0)
                 record_buffer << note
-                pi.find_pattern(PatternElement.new(note))
+                hmm.add(note)
               end
             end
           else
