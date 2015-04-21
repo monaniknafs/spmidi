249267d1b1136c553642f789cb6446e0a9ce0561
mona.niknafs <mona.niknafs@hotmail.com>
2015-04-21 17:22:01 +0100

    Sonic Pi element class used for evaluation

---
+++ lib/spmidi/sp_element.rb
@@ -0,0 +1,42 @@
+module SPMidi
+  class SPElement
+    attr_accessor :ts, :rel_ts, :pitch
+
+    def initialize(h)
+      @ts = h[:ts]
+      @rel_ts = h[:rel_ts]
+      @pitch = h[:pitch]
+    end
+
+    def ==(el)
+      return  @ts == el.ts && 
+              @rel_ts == el.rel_ts && 
+              @pitch == el.pitch
+    end
+
+    def sp_print
+      puts "sleep #{@rel_ts}"
+      puts "play #{pitch}"
+    end
+
+    def sp_string
+      return "sleep #{@rel_ts}\nplay #{pitch}"
+    end
+
+    def lock_rel_ts(incr)
+      # min_time_element in milliseconds
+      # lock relative ts to structure
+      r = @rel_ts % incr
+      return @rel_ts - r + (r >= incr / 2.0 ? incr : 0)
+    end       
+
+    def sp_drum_print
+      s = Samples.new
+      reg_pitch = @pitch - 48
+      if (reg_pitch).between?(0,17) 
+        puts "sleep #{@rel_ts/500.0}"
+        puts "sample :#{s.drum(reg_pitch)}"
+      end
+    end
+  end
+end
