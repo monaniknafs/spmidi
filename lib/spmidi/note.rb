caf4faa44e86e460f5319b074469a9b7711c3baa
mona.niknafs <mona.niknafs@hotmail.com>
2015-04-26 15:18:17 +0100

    wild timestamp provisions, used for first element timestamp fix and type B errors

--- lib/spmidi/note.rb
+++ lib/spmidi/note.rb
@@ -3,22 +3,36 @@ require_relative 'samples'
 
 module SPMidi
   class Note
-    attr_accessor :data, :ts, :rel_ts
+    attr_accessor :data, :ts, :rel_ts, :wild_ts
     
     def initialize(data, ts, rel_ts)
       @data = data # an array [status, pitch, velocity]
       @ts = ts # timestamp relative to input connection
       @rel_ts = rel_ts # timestamp relative to prev note played
+      @wild_ts = !@rel_ts # false if rel_ts exists; i.e is a Float
     end
 
     def ==(note)
-      # ts unique to each note, not tested
-      @data[1] == note.data[1] && 
-      @rel_ts == note.rel_ts
+      # ts unique to each note, not tested 
+      if note == nil 
+        false
+      elsif @wild_ts
+        @data[1] == note.data[1]
+        # for now leave it wild
+      else
+        @data[1] == note.data[1] && 
+        @rel_ts == note.rel_ts
+      end
     end
 
+    # bear in mind eql? isn't overridden
+
     def hash
-      @data.dup.concat([@rel_ts]).hash
+      @data.dup.concat([@ts]).hash
+    end
+
+    def set_rel_ts(new_rel_ts)
+      @rel_ts = new_rel_ts
     end
 
     def down_octave(x=1) 
@@ -51,14 +65,24 @@ module SPMidi
 
     def lock_to_structure(rel_ts_or_ts, element)
       # min_time_element in milliseconds
-      timestamp = (rel_ts_or_ts == "rel_ts" ? @rel_ts : @ts)
-      r = timestamp % element
-      rounded_ts = timestamp - r + (r >= element / 2.0 ? element : 0)
-      return rounded_ts
+      if !@wild_ts
+        timestamp = (rel_ts_or_ts == "rel_ts" ? @rel_ts : @ts)
+        r = timestamp % element
+        rounded_ts = timestamp - r + (r >= element / 2.0 ? element : 0)
+        return rounded_ts
+      else 
+        puts "can't lock non-existent timestamp of a wild ts element, honey!"
+        return
+      end
     end
 
     def lock_rel_ts(element)
-      @rel_ts = lock_to_structure("rel_ts", element)
+      if !@wild_ts
+        @rel_ts = lock_to_structure("rel_ts", element)
+      else 
+        puts "can't lock non-existent timestamp of a wild ts element, honey!"
+        return
+      end     
     end
 
     def lock_ts(element)
@@ -66,25 +90,35 @@ module SPMidi
     end
 
     def print
-      puts "data: #{data}, rel ts: #{rel_ts}"
+      if !@wild_ts
+        puts "data: #{data}, rel ts: #{rel_ts}"
+      else 
+        puts "data: #{data}, rel ts: *"
+      end
     end
 
     def sp_number_print()
-      puts "sleep #{@rel_ts/1000}"
-      puts "play :#{@data[1]}"
+      if !@wild_ts
+        puts "sleep #{@rel_ts/1000}"
+        puts "play :#{@data[1]}"
+      end
     end
 
     def sp_letter_print()
       # TODO: what units does sonic pi use between notes?
       # answer: ts relative to beats per minute; fix once integrated
-      puts "sleep #{@rel_ts/1000*2.0}"
-      puts "play :#{note_letter()}"
+      if !@wild_ts
+        puts "sleep #{@rel_ts/1000*2.0}"
+        puts "play :#{note_letter()}"
+      end
     end
 
     def sp_electric_print()
-      s = Samples.new
-      puts "sleep #{@rel_ts/1000}"
-      puts "sample :#{s.electric(@data[1])}"
+      if !@wild_ts
+        s = Samples.new
+        puts "sleep #{@rel_ts/1000}"
+        puts "sample :#{s.electric(@data[1])}"
+      end
     end
 
     def sp_ambi_print()
@@ -98,22 +132,26 @@ module SPMidi
     end
 
     def sp_ambi_print()
-      s = Samples.new
-      pitch = data[1]
-      reg_pitch = pitch - 48
-      if (reg_pitch).between?(0,7) 
-        puts "sleep #{@rel_ts/1000}"
-        puts "sample :#{s.bass(reg_pitch)}"
+      if !@wild_ts
+        s = Samples.new
+        pitch = data[1]
+        reg_pitch = pitch - 48
+        if (reg_pitch).between?(0,7) 
+          puts "sleep #{@rel_ts/1000}"
+          puts "sample :#{s.bass(reg_pitch)}"
+        end
       end
     end
 
     def sp_drum_print()
-      s = Samples.new
-      pitch = data[1]
-      reg_pitch = pitch - 48
-      if (reg_pitch).between?(0,17) 
-        puts "sleep #{@rel_ts/1000}"
-        puts "sample :#{s.drum(reg_pitch)}"
+      if !@wild_ts
+        s = Samples.new
+        pitch = data[1]
+        reg_pitch = pitch - 48
+        if (reg_pitch).between?(0,17) 
+          puts "sleep #{@rel_ts/1000}"
+          puts "sample :#{s.drum(reg_pitch)}"
+        end
       end
     end
   end
--- lib/spmidi/pattern_element.rb
+++ lib/spmidi/pattern_element.rb
@@ -9,13 +9,17 @@ module SPMidi
         @mean_ts = args[1]
       elsif args.size == 1
         @data = args[0].data
-        @mean_ts = args[0].rel_ts
+        if args[0].wild_ts 
+          @mean_ts = false
+        else
+          @mean_ts = args[0].rel_ts
+        end
       else
         puts 'error: pattern element initialize takes 1 or 2 arguments'
         return
       end
 
-      @timestamps = [@mean_ts]
+      @timestamps = nil
       @n = 1 # number of (rel) timestamp elements in distribution
       @th = 3.0 # num of standard deviations around mean_ts tolerated for equality of ts
       @sp_ts = nil
@@ -24,6 +28,7 @@ module SPMidi
       @wild_ts = !@mean_ts ? true : false # otherwise @data is integer
       if !@wild_ts
         @sd = (44 + 0.0447*(@mean_ts - 637)).abs
+        @timestamps = [@mean_ts]
       end
     end
 
@@ -41,13 +46,14 @@ module SPMidi
         return false
       end
 
-      if @wild_ts
+      if @wild_ts || element.wild_ts
         if !element.wild_ts
           @wild_ts = false
           prev_mean_ts = element.mean_ts
           @n += element.n
           @timestamps = element.timestamps 
-          for i in (0..@n-1)
+          for i in (0..@n-2)
+            puts "ts number #{i}"
             @timestamps << element.mean_ts
           end
           @mean_ts = prev_mean_ts + (element.mean_ts - prev_mean_ts)/@n
@@ -72,16 +78,26 @@ module SPMidi
       end 
     end
 
+    def set_mean_ts(new_ts)
+      @mean_ts = new_ts
+    end
+
     def eql?(element)
       # for use when don't want to add to distribution
       if element == nil
         return false
       end
+
       if !@wild_pitch && !element.wild_pitch
         if element.data[1] != @data[1]
           return false
         end
       end
+
+      if @wild_ts || element.wild_ts
+        return true
+      end
+
       # rel tstamps are normally distributed,
       # using line of regression, approx standard deviation:
       if (element.mean_ts >= @mean_ts - @th*@sd && element.mean_ts <= @mean_ts + @th*@sd)
@@ -94,17 +110,30 @@ module SPMidi
 
     def print
       if @wild_pitch
-        puts "data: *, rel ts: #{mean_ts}"
+        if @wild_ts
+          puts "data: *, rel ts: *"
+        else
+          puts "data: *, rel ts: #{mean_ts}"
+        end
       else
-        puts "data: #{data}, rel ts: #{mean_ts}"
+        if @wild_ts
+          puts "data: #{data}, rel ts: *"
+        else
+          puts "data: #{data}, rel ts: #{mean_ts}"
+        end
       end
     end
 
     def lock_mean_ts(incr)
       # min_time_element in milliseconds
       # lock relative ts to structure
-      r = @mean_ts % incr
-      return @mean_ts - r + (r >= incr / 2.0 ? incr : 0)
+      if !@wild_ts
+        r = @mean_ts % incr
+        return @mean_ts - r + (r >= incr / 2.0 ? incr : 0)
+      else
+        puts "can't lock non-existent mean_ts"
+        return
+      end
     end  
 
     def sp_ts(incr)
